4ecace7f7633de4219adb0f4d51e0da4af753ac3
admin <admin@DESKTOP-QVFGQDO>
2024-12-09 06:17:11 +0700

    Xem số lượng hàng trong giỏ hàng(Đơn vị: VND html(&#8363))

--- src/main/java/vn/iostar/Controller/HomeController.java
+++ src/main/java/vn/iostar/Controller/HomeController.java
@@ -31,6 +31,7 @@ import jakarta.servlet.http.HttpSession;
 import vn.iostar.model.Category;
 import vn.iostar.model.Product;
 import vn.iostar.model.UserDtls;
+import vn.iostar.service.CartService;
 import vn.iostar.service.CategoryService;
 import vn.iostar.service.ProductService;
 import vn.iostar.service.UserService;
@@ -38,34 +39,40 @@ import vn.iostar.util.CommonUtil;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	private CategoryService categoryService;
-	
+
 	@Autowired
 	private ProductService productService;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private CommonUtil commonUtil;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
+
+	@Autowired
+	private CartService cartService;
+
 	@ModelAttribute
 	public void getUserDetails(Principal p, Model m) {
 		if (p != null) {
 			String email = p.getName();
 			UserDtls userDtls = userService.getUserByEmail(email);
 			m.addAttribute("user", userDtls);
+			// Thêm phần đếm số lượng sản phẩm trong giỏ hàng
+			Integer countCart = cartService.getCountCart(userDtls.getId());
+			m.addAttribute("countCart", countCart);
 		}
-		
+
 		List<Category> allActiveCategory = categoryService.getAllActiveCategory();
 		m.addAttribute("categorys", allActiveCategory);
 	}
-	
+
 	@GetMapping("/")
 	public String index() {
 		return "index";
@@ -80,7 +87,7 @@ public class HomeController {
 	public String register() {
 		return "register";
 	}
-	
+
 	@GetMapping("/products")
 	public String products(Model m, @RequestParam(value = "category", defaultValue = "") String category) {
 		List<Category> categories = categoryService.getAllActiveCategory();
@@ -90,30 +97,30 @@ public class HomeController {
 		m.addAttribute("paramValue", category);
 		return "product";
 	}
-	
+
 	@GetMapping("/product/{id}")
 	public String product(@PathVariable int id, Model m) {
 		Product productById = productService.getProductById(id);
 		m.addAttribute("product", productById);
 		return "view_product";
 	}
-	
+
 	@PostMapping("/saveUser")
 	public String saveUser(@ModelAttribute UserDtls user, @RequestParam("img") MultipartFile file, HttpSession session)
 			throws IOException {
-		
+
 		String imageName = file.isEmpty() ? "default.jpg" : file.getOriginalFilename();
 		user.setProfileImage(imageName);
 		UserDtls saveUser = userService.saveUser(user);
-		
+
 		if (!ObjectUtils.isEmpty(saveUser)) {
 			if (!file.isEmpty()) {
 				File saveFile = new ClassPathResource("static/img").getFile();
-				
+
 				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "profile_img" + File.separator
 						+ file.getOriginalFilename());
-				
-				//System.out.println(path);
+
+				// System.out.println(path);
 				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
 			}
 			session.setAttribute("succMsg", "Register successfully");
@@ -122,28 +129,28 @@ public class HomeController {
 		}
 		return "redirect:/register";
 	}
-	
+
 //	Forgot Password Code 
-	
+
 	@GetMapping("/forgot-password")
 	public String showForgotPassword() {
 		return "forgot_password.html";
 	}
-	
+
 	@PostMapping("/forgot-password")
 	public String processForgotPassword(@RequestParam String email, HttpSession session, HttpServletRequest request)
 			throws UnsupportedEncodingException, MessagingException {
-		
+
 		UserDtls userByEmail = userService.getUserByEmail(email);
-		
+
 		if (ObjectUtils.isEmpty(userByEmail)) {
 			session.setAttribute("errorMsg", "Invalid email");
 		} else {
-			
+
 			String resetToken = UUID.randomUUID().toString();
-			
+
 			userService.updateUserResetToken(email, resetToken);
-			
+
 			// Generate URL :
 			// http://localhost:8082/reset-password?token=sfgdbgfswegfbdgfewgvsrg
 			String url = CommonUtil.generateUrl(request) + "/reset-password?token=" + resetToken;
@@ -156,7 +163,7 @@ public class HomeController {
 		}
 		return "redirect:/forgot-password";
 	}
-	
+
 	@GetMapping("/reset-password")
 	public String showResetPassword(@RequestParam String token, HttpSession session, Model m) {
 		UserDtls userByToken = userService.getUserByToken(token);
@@ -167,11 +174,11 @@ public class HomeController {
 		m.addAttribute("token", token);
 		return "reset_password";
 	}
-	
+
 	@PostMapping("/reset-password")
 	public String resetPassword(@RequestParam String token, @RequestParam String password, HttpSession session,
 			Model m) {
-		
+
 		UserDtls userByToken = userService.getUserByToken(token);
 		if (userByToken == null) {
 			m.addAttribute("errorMsg", "Your link is invalid or expired !!");
@@ -180,11 +187,11 @@ public class HomeController {
 			userByToken.setPassword(passwordEncoder.encode(password));
 			userByToken.setResetToken(null);
 			userService.updateUser(userByToken);
-			//session.setAttribute("succMsg", "Password change successfully");
-			m.addAttribute("msg","Password change successfully");
-			
+			// session.setAttribute("succMsg", "Password change successfully");
+			m.addAttribute("msg", "Password change successfully");
+
 			return "message";
 		}
 	}
-	
+
 }
--- src/main/java/vn/iostar/Controller/UserController.java
+++ src/main/java/vn/iostar/Controller/UserController.java
@@ -25,38 +25,66 @@ import vn.iostar.service.UserService;
 public class UserController {
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private CategoryService categoryService;
-	
+
 	@Autowired
 	private CartService cartService;
-	
+
 	@GetMapping("/")
 	public String home() {
 		return "user/home";
 	}
-	
+
 	@ModelAttribute
 	public void getUserDetails(Principal p, Model m) {
 		if (p != null) {
 			String email = p.getName();
 			UserDtls userDtls = userService.getUserByEmail(email);
 			m.addAttribute("user", userDtls);
+			// Thêm phần đếm số lượng sản phẩm trong giỏ hàng
+			Integer countCart = cartService.getCountCart(userDtls.getId());
+			m.addAttribute("countCart", countCart);
 		}
 		List<Category> allActiveCategory = categoryService.getAllActiveCategory();
 		m.addAttribute("categorys", allActiveCategory);
 	}
-	
+
 	@GetMapping("/addCart")
-	public String addToCart(@RequestParam Integer pid, @RequestParam Integer uid,HttpSession session) {
+	public String addToCart(@RequestParam Integer pid, @RequestParam Integer uid, HttpSession session) {
 		Cart saveCart = cartService.saveCart(pid, uid);
-		
+
 		if (ObjectUtils.isEmpty(saveCart)) {
 			session.setAttribute("errorMsg", "Product add to cart failed");
-		}else {
+		} else {
 			session.setAttribute("succMsg", "Product added to cart");
 		}
 		return "redirect:/product/" + pid;
+	}
+
+	@GetMapping("/cart")
+	public String loadCartPage(Principal p, Model m) {
+
+		UserDtls user = getLoggedInUserDetails(p);
+		List<Cart> carts = cartService.getCartsByUser(user.getId());
+		m.addAttribute("carts", carts);
+		if (carts.size() > 0) {
+			Double totalOrderPrice = carts.get(carts.size() - 1).getTotalOrderPrice();
+			m.addAttribute("totalOrderPrice", totalOrderPrice);
+		}
+		return "/user/cart";
+	}
+
+	@GetMapping("/cartQuantityUpdate")
+	public String updateCartQuantity(@RequestParam String sy, @RequestParam Integer cid) {
+		cartService.updateQuantity(sy, cid);
+		return "redirect:/user/cart";
+	}
+
+	private UserDtls getLoggedInUserDetails(Principal p) {
+		String email = p.getName();
+		UserDtls userDtls = userService.getUserByEmail(email);
+		return userDtls;
 	}
 }
