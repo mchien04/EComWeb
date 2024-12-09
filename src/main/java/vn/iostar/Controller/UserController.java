67e2235268d26c07960571007c5fb7d3f4fe92ee
mchien04 <chienletranminh@gmail.com>
2024-12-09 13:24:08 +0700

    update profile & change password for user

--- src/main/java/vn/iostar/Controller/UserController.java
+++ src/main/java/vn/iostar/Controller/UserController.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.servlet.http.HttpSession;
 import vn.iostar.model.Cart;
@@ -43,6 +45,9 @@ public class UserController {
 	
 	@Autowired
 	private CommonUtil commonUtil;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@GetMapping("/")
 	public String home() {
@@ -148,8 +153,10 @@ public class UserController {
 		ProductOrder updateOrder = orderService.updateOrderStatus(id, status);
 		
 		try {
+			
 			commonUtil.sendMailForProductOrder(updateOrder, status);
 		} catch (Exception e) {
+			
 			e.printStackTrace();
 		}
 		if (!ObjectUtils.isEmpty(updateOrder)) {
@@ -158,6 +165,42 @@ public class UserController {
 			session.setAttribute("errorMsg", "status not updated");
 		}
 		return "redirect:/user/user-orders";
+	}
+	
+	@GetMapping("/profile")
+	public String profile() {
+		return "/user/profile";
+	}
+	
+	@PostMapping("/update-profile")
+	public String updateProfile(@ModelAttribute UserDtls user, @RequestParam MultipartFile img, HttpSession session) {
+		UserDtls updateUserProfile = userService.updateUserProfile(user, img);
+		if (ObjectUtils.isEmpty(updateUserProfile)) {
+			session.setAttribute("errorMsg", "Profile not updated");
+		} else {
+			session.setAttribute("succMsg", "Profile Updated");
+		}
+		return "redirect:/user/profile";
+	}
+	
+	@PostMapping("/change-password")
+	public String changePassword(@RequestParam String newPassword, @RequestParam String currentPassword, Principal p,
+			HttpSession session) {
+		UserDtls loggedInUserDetails = getLoggedInUserDetails(p);
+		boolean matches = passwordEncoder.matches(currentPassword, loggedInUserDetails.getPassword());
+		if (matches) {
+			String encodePassword = passwordEncoder.encode(newPassword);
+			loggedInUserDetails.setPassword(encodePassword);
+			UserDtls updateUser = userService.updateUser(loggedInUserDetails);
+			if (ObjectUtils.isEmpty(updateUser)) {
+				session.setAttribute("errorMsg", "Password not updated !! Error in server");
+			} else {
+				session.setAttribute("succMsg", "Password Updated sucessfully");
+			}
+		} else {
+			session.setAttribute("errorMsg", "Current Password incorrect");
+		}
+		return "redirect:/user/profile";
 	}
 	
 }
--- src/main/java/vn/iostar/service/UserService.java
+++ src/main/java/vn/iostar/service/UserService.java
@@ -2,6 +2,8 @@ package vn.iostar.service;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import vn.iostar.model.UserDtls;
 
 public interface UserService {
@@ -27,5 +29,7 @@ public interface UserService {
 	public UserDtls getUserByToken(String token);
 	
 	public UserDtls updateUser(UserDtls user);
+	
+	public UserDtls updateUserProfile(UserDtls user,MultipartFile img);
 	
 }
